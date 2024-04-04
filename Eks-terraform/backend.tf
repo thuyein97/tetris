cd0d13278fa5a6a23e20459e91f8509015a7ca13
Thu Yein Tin <111072736+thuyein97@users.noreply.github.com>
2024-04-04 08:33:41 +0630

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
+    bucket = "mynewbucket9799" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
